072cb1f99c70353d4fdd0dce56a0277950a637e5
Prakhar <playingprakhar122@gmail.com>
2025-12-15 01:37:08 +0530

    improved UI and fixed problems

--- frontend/src/components/MessageArea.jsx
+++ frontend/src/components/MessageArea.jsx
@@ -1,6 +1,9 @@
-
 function MessageArea() {
-  return <div className="lg:w-70%] w-full h-full">Sidebar</div>;
+  return (
+    <div className="lg:w-60%] w-full h-full flex justify-center items-center">
+      Message Area
+    </div>
+  );
 }
 
 export default MessageArea;
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import { UserContext } from "../context/ContextApi";
 
 function Sidebar() {
-  const { serverUrl, othersLoading } = useContext(UserContext);
+  const { serverUrl } = useContext(UserContext);
 
   const dispatchRedux = useDispatch();
   const navigate = useNavigate();
@@ -32,19 +32,22 @@ function Sidebar() {
 
   const { userData, otherUserData } = useSelector((state) => state.user);
   return (
-    <div className="lg:w-[30%] w-full h-full bg-slate-200 border-r border-gray-300 p-1 relative select-none">
-      <div className="bg-[#20c7ff] w-full sm:h-[110px] h-[120px] rounded-full flex flex-col items-center text-center border-r-black-500 shadow-md mt-2">
-        <h1 className="text-2xl text-white font-lobster mt-2 font-semibold shadow-sm shadow-blue p-1 px-2 rounded-2xl">
-          Pakhiii | Chatting
-        </h1>
-        <div className="w-full flex justify-between items-center px-5 mt-2">
+    <div className="lg:w-[40%] w-full h-full bg-slate-200 border-r border-gray-300 p-1 relative select-none">
+      <h1 className="text-2xl text-white font-lobster mt-2 font-semibold shadow-md shadow-gray-400 p-1 px-2 rounded-2xl bg-[#20c7ff] w-fit m-auto">
+        Pakhiii | Chatting
+      </h1>
+      <div className="bg-[#20c7ff] w-full sm:h-[70px] h-[60px] rounded-full flex flex-col items-center justify-center text-center border-r-black-500 shadow-md mt-2">
+        <div className="w-full flex justify-between items-center px-5">
           <h1 className="font-lobster -tracking-normal text-white font-bold text-[20px]">
             Hi,{" "}
             <span className="text-white font-normal text-[18px]">
-              {userData?.name}
+              {userData?.name || "user"}
             </span>
           </h1>
-          <div className="w-[45px] h-[45px] rounded-full overflow-visible relative">
+          <div
+            className="sm:w-[45px] sm:h-[45px] w-[35px] h-[35px] rounded-full overflow-visible relative cursor-pointer"
+            onClick={() => navigate("/profile")}
+          >
             <img
               src={userData?.image || emptyImage}
               alt="profile"
@@ -65,23 +68,46 @@ function Sidebar() {
         <IoIosSearch size={30} />
       </div>
       {/* Online users strip */}
-<div className="w-full overflow-x-auto overflow-y-hidden mt-3 thin-scrollbar">
-  <div className="flex gap-2 px-1 h-[60px] items-center">
-    {otherUserData?.map((user) => (
-      <div
-        key={user._id}
-        className="w-[50px] h-[50px] rounded-full shrink-0 relative"
-      >
+      {/* <div className="w-full overflow-x-auto overflow-y-hidden mt-3 thin-scrollbar">
+        <div className="flex gap-2 px-1 h-[60px] items-center">
+          {otherUserData?.map((user) => (
+            <div
+              key={user._id}
+              className="w-[50px] h-[50px] rounded-full shrink-0 relative"
+            >
+              <img
+                src={user.image || emptyImage}
+                className="w-full h-full object-cover rounded-full"
+              />
+              <span className="absolute -bottom-0.5 right-0">
+                <GoDotFill color="#929693" />
+              </span>
+            </div>
+          ))}
+        </div>
+      </div> */}
+      <div className="w-full h-full flex flex-col items-center gap-2 mt-3 cursor-pointer ">
+  {otherUserData?.map((user) => (
+    <div
+      key={user._id}
+      className="w-full h-[60px] flex items-stretch bg-white shadow-md shadow-gray-400 rounded-2xl gap-3 px-1 hover:bg-gray-100 duration-150"
+    >
+      {/* Image wrapper */}
+      <div className="w-[50px] h-full flex items-center shrink-0">
         <img
           src={user.image || emptyImage}
-          className="w-full h-full object-cover rounded-full"
+          className="w-[50px] h-[50px] object-cover rounded-full"
         />
-        <span className="absolute -bottom-0.5 right-0">
-          <GoDotFill color="#929693" />
-        </span>
       </div>
-    ))}
-  </div>
+
+      {/* Text wrapper */}
+      <div className="flex flex-col justify-start pt-1">
+        <h1 className="text-sm font-semibold text-[16px]">
+          {user?.name}
+        </h1>
+      </div>
+    </div>
+  ))}
 </div>
 
       <button
