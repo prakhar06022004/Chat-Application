be5e8bf64a78e9564c1cfdc7caa3d45856a86dc3
Prakhar <playingprakhar122@gmail.com>
2025-12-17 23:49:26 +0530

    Now creating message model and conversation model for our chats

---
+++ backend/models/messageModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true, 
+    },
+  },
+  { timestamps: true }
+);
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
